af6ace69db5f47746e758feb350318cf001ae7a6
adrianpopescupx <adrianpopescupx@gmail.com>
2023-09-05 14:45:21 +0300

    App Finished

--- app/src/main/java/com/example/tippster/MainActivity.kt
+++ app/src/main/java/com/example/tippster/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.tippster
 
 import android.annotation.SuppressLint
-import android.nfc.Tag
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
@@ -19,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var tvTipPercent: TextView
     private lateinit var tvTipAmount: TextView
     private lateinit var tvTotalAmount: TextView
+    private lateinit var tvTipDescription : TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -27,15 +27,17 @@ class MainActivity : AppCompatActivity() {
         tvTipAmount = findViewById(R.id.tvTipAmount)
         tvTipPercent = findViewById(R.id.TipP)
         tvTotalAmount = findViewById(R.id.tvTotalAmount)
-
+        tvTipDescription = findViewById(R.id.tvTipDesctiption)
         seekBarTip.progress = initialTipPercent
         tvTipPercent.text = "$initialTipPercent%"
+
         seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             @SuppressLint("SetTextI18n")
             override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
                 Log.i(TAG, "onprogressChanged $p1")
                 tvTipPercent.text = "$p1%";
                 computeTipAndTotal()
+                uptadeTipDescription(p1)
             }
 
             override fun onStartTrackingTouch(p0: SeekBar?) {
@@ -73,7 +75,21 @@ class MainActivity : AppCompatActivity() {
         val totalAmount = baseAmount + tipAmount
 
         // 3. Updating the UI
-        tvTipAmount.text = "%2f".format(tipAmount)
-        tvTotalAmount.text = "%2f".format(totalAmount)
+        tvTipAmount.text = "%.2f".format(tipAmount)
+        tvTotalAmount.text = "%.2f".format(totalAmount)
+    }
+
+    private fun uptadeTipDescription(tipPercentage: Int) {
+        var tipDescription = ""
+        if (tipPercentage >= 0 && tipPercentage <= 9) {
+            tipDescription = "Poor"
+        }   else if (tipPercentage > 9 && tipPercentage < 15) {
+            tipDescription = "Acceptable"
+        }   else if (tipPercentage > 14 && tipPercentage < 20) {
+            tipDescription = "Good"
+        }   else if (tipPercentage > 20 && tipPercentage < 25) {
+            tipDescription = "Great"
+        }   else tipDescription = "Fantastic"
+        tvTipDescription.text = tipDescription
     }
 }
