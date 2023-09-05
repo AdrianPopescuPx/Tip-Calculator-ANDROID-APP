be713966d46ec82a4a03a6feb9379cd99f623bfa
adrianpopescupx <adrianpopescupx@gmail.com>
2023-09-05 11:49:06 +0300

    Tip Precentage Bar Code Started

--- app/src/main/java/com/example/tippster/MainActivity.kt
+++ app/src/main/java/com/example/tippster/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.tippster
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var etBaseAmount: EditText
+    private lateinit var seekBarTip: SeekBar
+    private lateinit var tvTipPercent: TextView
+    private lateinit var tvTipAmount: TextView
+    private lateinit var tvTotalAmount: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        etBaseAmount = findViewById(R.id.etBaseAmount)
+        seekBarTip = findViewById(R.id.seekBarAmount)
+        tvTipAmount = findViewById(R.id.tvTipAmount)
+        tvTipPercent = findViewById(R.id.TipP)
+        tvTotalAmount = findViewById(R.id.tvTotalAmount)
+        seekBarTip.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {
+            }   
+            override fun onStopTrackingTouch(p0: SeekBar?) {
+            }
+
+        })
     }
 }
